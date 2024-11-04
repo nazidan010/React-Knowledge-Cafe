4a541b31a63dfc116d882e72f9f98f5be89a396f
nazidan010 <nazidan010@gmail.com>
2024-11-04 18:39:55 +0600

    seceond commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import './App.css'
 import Header from './Components/Header/Header'
 import Blogs from './Components/Blogs/Blogs'
+import Bookmarks from './Components/Bookmarks/Bookmarks'
 
 function App() {
 
   return (
     <>
       <Header></Header>
+      <div className='md:flex'>
       <Blogs></Blogs>
+      <Bookmarks></Bookmarks>
+      </div>
     </>
   )
 }
